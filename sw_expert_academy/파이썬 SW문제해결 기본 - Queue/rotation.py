e4a9b65943d50d572ebefd371ea454febe421b10
im-shung <seoyoung1113@gmail.com>
2022-06-23 17:37:06 +0900

    :sparkles: [파이썬 S/W 문제해결 기본 - Queue] 회전

---
+++ sw_expert_academy/파이썬 SW문제해결 기본 - Queue/rotation.py
@@ -0,0 +1,22 @@
+# SW Expert Academy 
+# 097. [파이썬 S/W 문제해결 기본 - Queue] 6일차 - 회전 D2
+# https://swexpertacademy.com/main/learn/course/subjectDetail.do?courseId=AVuPDN86AAXw5UW6&subjectId=AWOVIoJqqfYDFAWg
+
+# import sys
+# sys.stdin = open("../sample_input.txt", "r")
+
+
+import queue
+
+T = int(input())
+
+for test_case in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = list(map(int, input().split()))
+    q = queue.Queue()
+    for i in arr:
+        q.put(i)
+    for i in range(M%N):
+        q.put(q.get())
+        
+    print(f"#{test_case} {q.get()}")
