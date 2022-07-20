8a8ffda2da86d2ab074e5a943e121c72cce49b0f
im-shung <seoyoung1113@gmail.com>
2022-07-20 14:15:59 +0900

    :poop: 다시 풀어야 함

---
+++ SWEA/최적화 01 문자열 탐색/5254_부분_문자열.py
@@ -0,0 +1,19 @@
+# SW Expert Academy 
+# 5254. [파이썬 S/W 문제해결 최적화] 1일차 - 부분 문자열 D3
+# https://swexpertacademy.com/main/learn/course/subjectDetail.do?courseId=AVuPDYSqAAbw5UW6&subjectId=AWUYM1Ca240DFAVT
+# 다시 풀어야함
+
+import sys
+sys.stdin = open("../sample_input.txt", "r")
+
+for test_case in range(1, int(input()) + 1):
+    N, string = input().split()
+    N = int(N)
+    prefix = set()
+    s = len(string)
+    for i in range(0,s): 
+        for j in range(i+1,s+1):
+            prefix.add(string[i:j])
+    prefix = list(prefix)
+    prefix.sort()
+    print(f"#{test_case} {prefix[N-1][0]} {len(prefix[N-1])}")
