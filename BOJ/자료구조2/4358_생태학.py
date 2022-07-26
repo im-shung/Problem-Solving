48b9e03fef8c3280829244aa138d83d31a40ae7a
im-shung <seoyoung1113@gmail.com>
2022-07-26 20:28:30 +0900

    :sparkles: [BOJ] 자료구조2 4358 구현

--- BOJ/자료구조2/4358_생태학.py
+++ BOJ/자료구조2/4358_생태학.py
@@ -1,5 +1,4 @@
 import sys
-sys.stdin = open("sample_input.txt","r")
 Dict = {}
 names = set()
 hq = []
@@ -16,4 +15,5 @@ names = [x for x in Dict.keys()]
 names.sort()
 for name in names:
     ratio = (Dict[name]/cnt) * 100
-    print(name,round(ratio,4))
+    # print(name,round(ratio,4)) # round함수는 예상치못한 결과가 발생하기 때문에 안전하지 않다.
+    print('%s %.4f'%(name,(Dict[name]/cnt) * 100))  
