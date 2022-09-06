4b5dbf66602a851629d372dd49c66b8e3f7eb18d
im-shung <seoyoung1113@gmail.com>
2022-09-06 23:26:21 +0900

    :sparkles: [2021 KAKAO BLIND RECRUITMENT] 순위 검색 solved

--- 프로그래머스/2021 KAKAO BLIND RECRUITMENT/순위_검색.py
+++ 프로그래머스/2021 KAKAO BLIND RECRUITMENT/순위_검색.py
@@ -1,65 +1,51 @@
 '''
 채점 결과
 정확성: 40.0
-효율성: 0.0
-합계: 40.0 / 100.0
+효율성: 60.0
+합계: 100.0 / 100.0
 '''
+
 def solution(info, query):
-    check = [[]*5 for _ in range(len(info))]
-    for idx,string in enumerate(info):
-        for s in string.split():
-            check[idx].append(s)
+    from collections import defaultdict
+
+    check = defaultdict(list)
+    for string in info:
+        s = string.split()
+        for lang in [s[0], "-"]:
+                for job in [s[1], "-"]:
+                    for career in [s[2], "-"]:
+                        for food in [s[3], "-"]:
+                            check[lang+job+career+food].append(int(s[4])) # 해시테이블 사용하기
+    
+    # 점수별 오름차순 정렬하기
+    for key in check.keys():
+        check[key].sort()
+
     answer = []
     for q in query:
-        type = q.split()
-        while "and" in type:
-            type.remove("and")
-        idx_list = list(filter(lambda x: int(check[x][-1]) >= int(type[-1]), range(len(check))))
-        cnt = 0
-        for idx in idx_list:
-            flag = False
-            for i in range(4):
-                if type[i] == '-': 
-                    flag = True
-                    continue
-                if type[i] == check[idx][i]:
-                    flag = True
-                else:
-                    flag = False
-                    break
-            if flag: 
-                cnt += 1
-        answer.append(cnt)
+        temp = q.replace(" and ","")
+        type,score = temp.split(); score = int(score)
+
+        score_list = check[type]
+        l = len(score_list)
+
+        # 선형탐색이 아닌 이진탐색 같은 빠른 방법 쓰기
+        low, high, point = 0, l - 1, l
+        while low <= high:
+            mid = (low+high) // 2
+            if score <= score_list[mid]:
+                point = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        answer.append(l-point)
+
     return answer
 
 
-# 실행용 코드
-# info = ["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"]
-# query = ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]
-
-# check = [[]*5 for _ in range(len(info))]
-# for idx,string in enumerate(info):
-#     for s in string.split():
-#         check[idx].append(s)
-# answer = []
-# for q in query:
-#     type = q.split()
-#     while "and" in type:
-#         type.remove("and")
-#     idx_list = list(filter(lambda x: int(check[x][-1]) >= int(type[-1]), range(len(check))))
-#     cnt = 0
-#     for idx in idx_list:
-#         flag = False
-#         for i in range(4):
-#             if type[i] == '-': 
-#                 flag = True
-#                 continue
-#             if type[i] == check[idx][i]:
-#                 flag = True
-#             else:
-#                 flag = False
-#                 break
-#         if flag: 
-#             cnt += 1
-#     answer.append(cnt)
-# print(answer)
+'''
+TIP(https://school.programmers.co.kr/questions/25368)
+1. info에 없는 조건들이 query에 있을 수 있습니다. 모든 경우의 수를 넣지 않으면 런타임 에러가 발생합니다.
+2. 단순히 봐도 info 50,000개 x query 100,000개 = 50억개. 하나씩 비교해선 답이 없습니다. 해시테이블을 사용합시다.
+3. 점수를 비교할 때조차 시간을 아껴야 통과합니다. 선형탐색 말고 이진탐색 같은 빠른 방법을 쓰세요!
+'''
