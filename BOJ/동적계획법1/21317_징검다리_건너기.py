20a8d94d6ab41ad17fa92caef05ba5f53a8c6de2
im-shung <seoyoung1113@gmail.com>
2022-10-01 12:52:24 +0900

    :poop: [BOJ] 21317 type error

---
+++ BOJ/동적계획법1/21317_징검다리_건너기.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open("sample_input.txt", "r")
+
+SMALL = 0
+BIG = 1
+INF = (1e9)
+
+n = int(input())
+jump = [(INF,INF)] + [tuple(map(int,input().split())) for _ in range(n-1)] # 1돌부터 n-1돌 점프 
+k = int(input())
+dp = [0] + [0] + [jump[1][0]] + [INF] * (n-2) # 0돌부터 n돌까지 거리
+
+# 작은 점프, 큰 점프로만 dp 구하기 
+for i in range(3, n + 1):
+    dp[i] = min(dp[i - 1] + jump[i - 1][SMALL], dp[i - 2] + jump[i - 2][BIG])
+
+# 매우 큰 점프 적용해보기
+min = dp[-1]
+for j in range(1,n-2):
+    dp2 = dp[:]
+    if dp2[j] + k < dp2[j + 3]:
+        dp2[j + 3] = dp2[j] + k
+        for i in range(j + 4, n):
+           dp2[i] = min(dp2[i - 1] + jump[i - 1][SMALL], dp2[i - 2] + jump[i - 2][BIG])
+        if min > dp2[-1]:
+            min = dp2[-1]
+print(min)
