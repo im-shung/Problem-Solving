a1dcd74f12f7c540eaca371fe87cd96456786bab
im-shung <seoyoung1113@gmail.com>
2022-11-08 19:42:02 +0900

    :recycle: [BOJ] 1238 시간 단축(948ms -> 192ms)

--- BOJ/1238.py
+++ BOJ/1238.py
@@ -4,11 +4,10 @@ input = sys.stdin.readline
 
 INF = 1e9
 
-def search(start, distance):
+def search(start, graph, distance):
     Q = []
-    heapq.heappush(Q, (0, start))
+    heapq.heappush(Q, [0, start])
     distance[start] = 0
-
     while Q:
         d, v = heapq.heappop(Q)
         if distance[v] < d:
@@ -17,25 +16,29 @@ def search(start, distance):
             dist = distance[v] + next_dist
             if dist < distance[next_v]:
                 distance[next_v] = dist
-                heapq.heappush(Q, (dist, next_v))
+                heapq.heappush(Q, [dist, next_v])
 
     return distance[X]
 
 if __name__ == '__main__':
     N, M, X = map(int, input().split())
+
     graph = [[] for _ in range(N + 1)]
-    distance = [[INF for _ in range(N + 1)] for _ in range(N + 1)]
+    graph_r = [[] for _ in range(N + 1)]
+
+    distance = [INF for _ in range(N + 1)]
+    distance_r = [INF for _ in range(N + 1)]
 
     for _ in range(M):
         s, e, dist = map(int, input().split())
         graph[s].append([dist, e])
+        graph_r[e].append([dist, s])
 
-    search(X, distance[X])
+    search(X, graph, distance)
+    search(X, graph_r, distance_r)
 
     max_dist = 0
     for v in range(1, N + 1):
-        if v == X: continue
-        max_dist = max(search(v, distance[v]) + distance[X][v], max_dist)
-
+        max_dist = max(max_dist, distance[v] + distance_r[v])
 
     print(max_dist)
