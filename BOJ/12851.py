c3e7237f37c21c72aaa84a91eafc8ee847f4ea11
im-shung <seoyoung1113@gmail.com>
2022-11-09 23:31:35 +0900

    :recycle: [BOJ] 12851-숨바꼭질2

--- BOJ/12851.py
+++ BOJ/12851.py
@@ -1,11 +1,11 @@
 from _collections import deque
 
-def bfs(memo, N, K):
+def bfs(dist, N, K):
     queue = deque()
     queue.append(N)
 
-    memo[N][0] = 0 # 가장 빠른 시간
-    memo[N][1] = 1 # 가장 빠른 시간으로 찾는 방법 수
+    dist[N][0] = 0 # 가장 빠른 시간
+    dist[N][1] = 1 # 가장 빠른 시간으로 찾는 방법 수
 
     while queue:
         x = queue.popleft()
@@ -13,16 +13,16 @@ def bfs(memo, N, K):
             continue
         for dx in (x + 1, x - 1, x * 2):
             if 0 <= dx < 100_001:
-                if memo[dx][0] == 0:
-                    memo[dx][0] = memo[x][0] + 1
-                    memo[dx][1] = memo[x][1]
+                if dist[dx][0] == -1:
+                    dist[dx][0] = dist[x][0] + 1
+                    dist[dx][1] = dist[x][1]
                     queue.append(dx)
-                elif memo[dx][0] == memo[x][0] + 1:
-                    memo[dx][1] += memo[x][1]
+                elif dist[dx][0] == dist[x][0] + 1:
+                    dist[dx][1] += dist[x][1]
 
 if __name__ == '__main__':
     N, K = map(int,input().split())
-    memo = [[0, 0] for _ in range(100_001)]
-    bfs(memo, N, K)
-    print(memo[K][0])
-    print(memo[K][1])
+    dist = [[-1, 0] for _ in range(100_001)]
+    bfs(dist, N, K)
+    print(dist[K][0])
+    print(dist[K][1])
