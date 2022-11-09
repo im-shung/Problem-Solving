064ca5840b114958c1f90f6fabc1c4b404dcdfdd
im-shung <seoyoung1113@gmail.com>
2022-11-09 13:05:53 +0900

    :recycle: [BOJ] 1697 _collections 사용하니까 더 빨라짐..

--- BOJ/1697.py
+++ BOJ/1697.py
@@ -1,29 +1,19 @@
-import sys
-sys.stdin = open("../sample_input.txt", "r")
+from _collections import deque
 
-from collections import deque
-
-INF = 1e9
-
-def bfs(X):
+def bfs(memo, N, K):
     Q = deque()
-    memo = [INF for _ in range(100_001)]
-    visited = [False for _ in range(100_001)]
-
-    Q.append(X)
-    memo[X] = 0
-    visited[X] = True
+    Q.append(N)
 
     while Q:
         x = Q.popleft()
-        if x == K: return memo[x]
-        for dx in [x + 1, x - 1, x * 2]:
-            if dx < 0 or dx > 100_000 or visited[dx]: continue
-            if memo[x] + 1 < memo[K]:
-                Q.append(dx)
+        if x == K:
+            return memo[x]
+        for dx in (x + 1, x - 1, x * 2):
+            if (0 <= dx < 100_001) and memo[dx] == 0:
                 memo[dx] = memo[x] + 1
-                visited[dx] = True
+                Q.append(dx)
 
 if __name__ == '__main__':
     N, K = map(int,input().split())
-    print(bfs(N))
+    memo = [0] * 100_001
+    print(bfs(memo, N, K))
