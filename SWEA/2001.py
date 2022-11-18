61ece97969b902c1e55edf40d5e94fd871a78c07
im-shung <seoyoung1113@gmail.com>
2022-11-19 00:23:08 +0900

    :sparkles: [SWEA] 2001-파리 퇴치 (누적합 풀이 추가)

--- SWEA/2001.py
+++ SWEA/2001.py
@@ -1,3 +1,4 @@
+# 완전탐색
 for t in range(1, int(input()) + 1):
     n, m = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(n)]
@@ -10,5 +11,22 @@ for t in range(1, int(input()) + 1):
                 tmp += sum(arr[i][y: y + m])
             result = max(result,tmp)
 
+
+    print(f'#{t} {result}')
+
+# 누적합
+for t in range(1, int(input()) + 1):
+    n, m = map(int, input().split())
+    arr = [[0 for _ in range(n + 1)]]
+    for _ in range(n):
+        arr.append([0] + list(map(int, input().split())))
+    result = 0
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            arr[i][j] = arr[i-1][j] + arr[i][j]
+
+    for i in range(1, n - m + 2):
+        for j in range(1, n - m + 2):
+            result = max(result, sum(arr[i + m - 1][j: j + m]) - sum(arr[i - 1][j: j + m]))
 
     print(f'#{t} {result}')
