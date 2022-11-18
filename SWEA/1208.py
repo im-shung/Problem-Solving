18691029b466dc759a36dc82413288c1af49b68c
im-shung <seoyoung1113@gmail.com>
2022-11-18 23:25:03 +0900

    :recycle: [SWEA] 1208-Flatten

--- SWEA/1208.py
+++ SWEA/1208.py
@@ -1,15 +1,11 @@
 for t in range(1, 11):
     n = int(input())
-    s = list(map(int, input().split()))
+    matrix = list(map(int, input().split()))
+    matrix.sort()
 
-    cnt, mx, mn = 0, 0, 0
-    while cnt <= n:
-        cnt += 1
-        mx, mn = max(s), min(s)
-        if mx - mn <= 1:
-            break
-        mx_index = list(filter(lambda x: s[x] == mx, range(100)))
-        mn_index = list(filter(lambda x: s[x] == mn, range(100)))
-        s[mx_index[0]] -= 1
-        s[mn_index[0]] += 1
-    print(f'#{t} {mx - mn}')
+    for _ in range(n):
+        matrix[0] += 1
+        matrix[-1] -= 1
+        matrix.sort()
+
+    print(f"#{t} {matrix[-1] - matrix[0]}")
