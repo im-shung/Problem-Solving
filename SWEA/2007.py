3cbc9cb9a2622c1bc2625ece5672afe6eb3a6e6c
im-shung <seoyoung1113@gmail.com>
2022-11-19 11:55:10 +0900

    :recycle: [SWEA] 2007-패턴 마디의 길이

--- SWEA/2007.py
+++ SWEA/2007.py
@@ -2,17 +2,11 @@ for t in range(1, int(input()) + 1):
     s = list(input())
     idx = 1
     tmp = s[0]
-
     while idx < len(s):
         if s[idx] == tmp[0]:
             n = len(tmp)
-            if ''.join(map(str,s[idx:idx + n])) == tmp:
+            if ''.join(map(str,s[idx: idx + n])) == tmp:
                 break
-            else:
-                tmp += s[idx]
-                idx += 1
-        else:
-            tmp += s[idx]
-            idx += 1
+        tmp += s[idx]
+        idx += 1
     print(f'#{t} {len(tmp)}')
-
