77fd012605573a9ad43ff751dc9e183789b6ec6d
im-shung <seoyoung1113@gmail.com>
2022-12-15 12:30:44 +0900

    :sparkles: [BOJ] 15650-N과 M (2)

---
+++ BOJ/15650.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+    public static int M;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        FastReader scan = new FastReader();
+        N = scan.netxInt();
+        M = scan.netxInt();
+
+        arr = new int[M];
+
+        dfs(0, -1);
+        System.out.println(sb);
+
+    }
+
+    static void dfs (int depth, int key) {
+
+        // 재귀 길이가 M과 같아지면 그래프 탐색과정에서 담은 배열을 출력
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            // 만약 해당 노드를 방문하지 않았다면?
+            if (i > key) {
+                arr[depth] = i + 1;     // 해당 깊이를 index로 하며 i + 1 값 저장
+                dfs(depth + 1, i);   // 다음 자식 노드 방문을 위해 depth + 1 증가시키면서 재귀호출
+            }
+        }
+    }
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int netxInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
