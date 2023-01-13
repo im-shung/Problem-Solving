abc7e6dd5ffb0920474cca0cfa5b15f830cb2d28
Seoyoung Lim <seoyoung1113@gmail.com>
2023-01-13 14:37:36 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 660 ms, Memory: 62192 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,76 +1,49 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-/*
-Tree 트리
-트리는 정점과 간선 그리고 다음 중 2개 이상을 만족하는 자료구조이다.
-1. 모두가 연결돼 있는 그래프
-2. 사이클이 존재하지 않음
-3. 정점의 개수는 간선 개수 + 1
- */
-
+/** 
+* 알고리즘 : 트리 
+* 1. 일반적인 입력을 통해 트리를 구성하면 안된다.
+* 2. bfs 또는 dfs와 같은 탐색을 통한 트리 구현
+* 3. 트리 구현방법중 부모의 노드를 구하는 방식을 사용한다.
+*/
 public class Main {
-
-    static int N;
-    static ArrayList<Integer>[] adj;
-    static int[] parent;
-    static boolean[] visit;
-
-    static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-
-        // 인접 리스트 구성하기
-        adj = new ArrayList[N + 1];
-        parent = new int[N + 1];
-        visit = new boolean[N + 1];
-
-        // 정점 1 ~ N
-        for (int i = 1; i <= N; i++) {
-            adj[i] = new ArrayList<>();
-        }
-
-        // 간선 N -1 개
-        for (int i = 1; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            // 누가 자식이고 부모인지 모르는 상태로 시작한다.
-            adj[a].add(b);
-            adj[b].add(a);
-        }
-    }
-
-
-    // dfs(x, par)  := 정점 x 의 부모가 par 였고, x의 children 들을 찾아주는 함수
-    static void dfs(int x, int par) {
-        parent[x] = par;
-        visit[x] = true;
-
-        for (int y : adj[x]) {
-            if (y == par || visit[y]) {
-                continue;
-            }
-            dfs(y, x);
-            parent[y] = x;
-        }
-    }
-
-    static void pro() {
-        // 1번 정점이 ROOT 이므로, 여기서 시작해서 Tree의 구조를 파악하자.
-        dfs(1, -1);
-
-        // 정답 출력하기
-        for (int i = 2; i <= N; i++) {
-            System.out.println(parent[i]);
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        input();
-        pro();
-    }
-
+	static StringBuilder sb = new StringBuilder();
+	static int[] p;
+	static List<Integer>[] adj;
+	
+	static void dfs(int curr) {
+		for(int i = 0; i<adj[curr].size(); i++) {
+			int next = adj[curr].get(i);
+			if(p[curr] == next) continue;
+			p[next] = curr;
+			dfs(next);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		adj = new List[n + 1];
+		p = new int[n + 1];
+		
+		for(int i = 1; i<=n; i++) {
+			adj[i] = new ArrayList<>();
+		}
+		
+		for(int i = 1; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			adj[u].add(v);
+			adj[v].add(u);
+		}
+		
+		dfs(1);
+		for(int i = 2; i<=n; i++) {
+			sb.append(p[i]).append('\n');
+		}
+		System.out.println(sb);
+	}
 }
