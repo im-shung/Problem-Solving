751120f3908148a5844adcf1ce7d4d6cffdf8bd3
Seoyoung Lim <seoyoung1113@gmail.com>
2023-02-03 23:53:36 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 148 ms, Memory: 14608 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -7,13 +7,17 @@ public class Main {
     static StringTokenizer st;
 
     static int N;
-    static int[] seq, dp;
-    static int result = 0;
+
+    static int[] seq;
+    static int[] dp;
 
     static void input() throws IOException {
+//        br = new BufferedReader(new FileReader("src/input.txt"));
+
         st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
+
         seq = new int[N];
         dp = new int[N];
 
@@ -21,30 +25,20 @@ public class Main {
         for (int i = 0; i < N; i++) {
             seq[i] = Integer.parseInt(st.nextToken());
         }
+
     }
 
-    static void pro() {
-        dp();
-        System.out.println(result);
-    }
-
-    static void dp() {
+    static void pro() throws IOException {
         for (int i = 0; i < N; i++) {
-            // 모든 부분수열의 길이는 '최소한 1 이상'
             dp[i] = 1;
-
-            // 0 ~ i 이전 원소들 탐색
-            for (int j = 0; j < i; j++) {
-
-                // j번째 원소가 i번째 원소보다 작으면서 i번째 dp가 j번째 dp+1 값보다 작은경우
-                if (seq[i] > seq[j] && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
+            for (int j = 0; j < i ; j++) {
+                if (seq[j] < seq[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
-        result = Arrays.stream(dp).max().getAsInt();
+        System.out.println(Arrays.stream(dp).max().getAsInt());
     }
-
 
     public static void main(String[] args) throws IOException {
         input();
@@ -52,3 +46,5 @@ public class Main {
     }
 
 }
+
+
