14fda9ef48d0be0af734b4a44523007d11050648
Seoyoung Lim <seoyoung1113@gmail.com>
2023-02-14 14:56:09 +0900

    [Gold IV] Title: N-Queen, Time: 2540 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -8,9 +8,10 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int N;
-    static List<Integer> column = new ArrayList<>(); // 열
-    static List<Integer> leftDi = new ArrayList<>(); // 왼쪽 대각선
-    static List<Integer> rightDi = new ArrayList<>(); // 오른쪽 대각선
+    static int[][] map;
+    static boolean[] column = new boolean[15]; // 열
+    static boolean[] leftDi = new boolean[30]; // 왼쪽 대각선
+    static boolean[] rightDi = new boolean[30]; // 오른쪽 대각선
     static int ans;
 
     public static void main(String[] args) throws IOException {
@@ -20,35 +21,32 @@ public class Main {
 
         dfs(1);
         System.out.println(ans);
-
     }
 
     static void dfs(int r) {
+
         // 목적지인가? - 마지막 행
         if (r == N + 1) {
             ans += 1;
         } else {
+
             // 갈 수 있는가?
             for (int c = 1; c <= N; c++) {
-                if (column.contains(c) || leftDi.contains(r + c) || rightDi.contains(r - c)) {
-                    continue;
-                }
+                if (column[c] || leftDi[r + c] || rightDi[r - c + N]) continue;
 
                 // 체크인
-                column.add(c);
-                leftDi.add(r + c);
-                rightDi.add(r - c);
+                column[c] = true;
+                leftDi[r + c] = true;
+                rightDi[r - c + N] = true;
 
                 // 간다.
                 dfs(r + 1);
 
                 // 체크아웃
-                column.remove(column.size() - 1);
-                leftDi.remove(leftDi.size() - 1);
-                rightDi.remove(rightDi.size() - 1);
+                column[c] = false;
+                leftDi[r + c] = false;
+                rightDi[r - c  + N] = false;
             }
         }
-
     }
-
 }
