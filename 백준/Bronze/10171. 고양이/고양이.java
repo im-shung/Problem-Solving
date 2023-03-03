c7cd0da6bebe16536b1e8fbd1e6748fc48b1e74f
Seoyoung Lim <seoyoung1113@gmail.com>
2023-03-03 17:33:51 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		/*
+		 \    /\
+		 )  ( ')
+		(  /  )
+		 \(__)|
+		 */
+		sb.append("\\    /\\").append("\n")
+		.append(" )  ( ')").append("\n")
+		.append("(  /  )").append("\n")
+		.append(" \\(__)|");
+		System.out.println(sb);
+	}
+}
