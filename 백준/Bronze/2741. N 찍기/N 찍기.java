71c742fef9a468eca1c00bd15cd4d93b5ba24f93
Seoyoung Lim <seoyoung1113@gmail.com>
2023-03-03 16:33:29 +0900

    [Bronze V] Title: N 찍기, Time: 212 ms, Memory: 18248 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());		
+		for (int i = 1; i <= N; i++) {
+			sb.append(i).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
