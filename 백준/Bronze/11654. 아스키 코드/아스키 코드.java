7dfaa31ce7098ed7c13a158bce615f758f2c7c89
Seoyoung Lim <seoyoung1113@gmail.com>
2023-03-03 15:52:33 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		char word = br.readLine().charAt(0);
+		System.out.println((int) word);
+	}
+}
