4484ed45e75b97fa4de0c1874dee4aa5b535fd19
Seoyoung Lim <seoyoung1113@gmail.com>
2023-03-03 17:44:28 +0900

    [Bronze V] Title: A×B, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		System.out.println(a * b);
+	}
+}
