e4bec338aa547d7ecf63b3c4f02e9b167492d027
Seoyoung Lim <seoyoung1113@gmail.com>
2023-03-03 16:41:44 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());		
+		// 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F
+		switch (N / 10) {
+		case 10:
+		case 9:
+			System.out.println("A");
+			break;
+		case 8:
+			System.out.println("B");
+			break;
+		case 7:
+			System.out.println("C");
+			break;
+		case 6:
+			System.out.println("D");
+			break;
+		default:
+			System.out.println("F");
+		}
+	}
+}
