5222f4858164a842f7686cbf79061ea1031c1277
Seoyoung Lim <seoyoung1113@gmail.com>
2023-03-09 11:33:40 +0900

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws Exception {
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int V = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		for (int i : arr) if (i == V) cnt++;
+		System.out.println(cnt);
+	}
+
+}
