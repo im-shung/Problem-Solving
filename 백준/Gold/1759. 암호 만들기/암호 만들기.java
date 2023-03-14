f163749200c3b2bf808768af60b8d882a0fb5f8f
Seoyoung Lim <seoyoung1113@gmail.com>
2023-03-14 15:58:01 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,80 +1,59 @@
+
+/*
+ * 조합문제
+ * 1) 중복 없이
+ * 2) 순서 고정 (알파벳이 암호에서 증가하는 순서)
+ */
+
 import java.io.*;
 import java.util.*;
-
-class Node {
-	int x; // row
-	int y; // column
-
-	public Node(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	@Override
-	public String toString() {
-		return "Node [x=" + x + ", y=" + y + "]";
-	}
-
-}
 
 public class Main {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
+	static int L, C; // 암호 길이, 문자 종류
+	static char[] answer, input;
 
-	static int N, M;
-	static char[] A, answer;
-	static boolean[] visit;
-
-	static void input() throws IOException {
+	public static void main(String[] args) throws Exception {
 		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		answer = new char[N];
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
 
+		answer = new char[L];
+		input = new char[C];
 		st = new StringTokenizer(br.readLine());
-		A = new char[M];
-		visit = new boolean[M];
-		for (int i = 0; i < M; i++) {
-			A[i] = st.nextToken().charAt(0);
+		for (int i = 0; i < C; i++) {
+			input[i] = st.nextToken().charAt(0);
 		}
-		Arrays.sort(A);
-	}
-
-	public static void dfs(int len, int idx) {
-		if (len == N) {
-			int mCount = 0, jCount = 0;
-			for (char a : answer) {
-				if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u')
-					mCount++;
-				else
-					jCount++;
-			}
-			if (mCount >= 1 && jCount >= 2)
-				sb.append(answer).append("\n");
-		} else {
-			for (int x = idx; x < M; x++) {
-				if (!visit[x]) {
-					visit[x] = true;
-					answer[len] = A[x];
-					dfs(len + 1, x);
-					visit[x] = false;
-				}
-			}
-		}
-	}
-	
-	public static void pro() {
-		dfs(0, 0);
-	}
-
-	public static void main(String[] args) throws IOException {
-		/* 입력 */
-		input();
-		/* 처리 */
-		pro();
-		/* 출력 */
+		Arrays.sort(input);
+		comb(0, 0);
 		System.out.println(sb);
 	}
+
+	// cnt := cnt번째 카드 뽑기
+	private static void comb(int cnt, int startIndex) {
+		if (cnt == L) {
+			int mo = 0, ja = 0;
+			for (char a : answer) {
+				if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u')
+					mo++;
+				else
+					ja++;
+			}
+			if (mo >= 1 && ja >= 2) {
+				for (char a : answer) 
+					sb.append(a);
+				sb.append("\n");
+			}
+
+			return;
+		}
+		for (int i = startIndex; i < C; i++) {
+			answer[cnt] = input[i];
+			comb(cnt + 1, i + 1);
+		}
+	}
+
 }
