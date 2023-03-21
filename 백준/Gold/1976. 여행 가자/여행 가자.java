4aca5310f00d3b553c841d996e75c6d0e80d18b4
Seoyoung Lim <seoyoung1113@gmail.com>
2023-03-21 17:15:28 +0900

    [Gold IV] Title: 여행 가자, Time: 196 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -8,14 +8,22 @@ public class Main {
 
 	static int N, M;
 	static int[] parent, plan;
+	static boolean isPossible = true;
 
-	public static void main(String[] args) throws Exception {
+	// 크기가 1인 서로소 집합
+	private static void make() {
+		for (int i = 0; i < N; i++) {
+			parent[i] = i;
+		}
+	}
+	
+	private static void input() throws Exception {
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
 		
 		parent = new int[N];
 		plan = new int[M];
-		init();
+		make();
 		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -26,32 +34,24 @@ public class Main {
 		}
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < M; i++) {
-			plan[i] = Integer.parseInt(st.nextToken());
+			plan[i] = Integer.parseInt(st.nextToken()) - 1;
 		}
-		boolean isPossible = true;
-		for (int i = 0; i < M - 1; i++) {
-			int a = plan[i] - 1;
-			int b = plan[i + 1] - 1;
-			if (!isSame(a, b)) {
-				isPossible = false;
-				break;
-			}
-		}
-		if (isPossible) System.out.println("YES");
-		else System.out.println("NO");
 	}
-
+	
+	// x와 y 노드가 같은 집합인지 확인
 	private static boolean isSame(int a, int b) {
 		int rootA = find(a);
 		int rootB = find(b);
 		return rootA == rootB;
 	}
 
+	// x 노드의 대표 노드 반환
 	private static int find(int x) {
 		if (parent[x] == x) return x;
 		return parent[x] = find(parent[x]);
 	}
 	
+	// x U y
 	private static void union(int a, int b) {
 		int rootA = find(a);
 		int rootB = find(b);
@@ -60,11 +60,24 @@ public class Main {
 		else parent[rootB] = rootA;
 	}
 
-	private static void init() {
-		for (int i = 0; i < N; i++) {
-			parent[i] = i;
+	private static void pro() {
+		for (int i = 0; i < M - 1; i++) {
+			int a = plan[i];
+			int b = plan[i + 1];
+			if (!isSame(a, b)) { // a 와 b가 연결되어 있는가?
+				isPossible = false;
+				break;
+			}
 		}
 	}
-
-
+	
+	public static void main(String[] args) throws Exception {
+		/* 입력 */
+		input();
+		/* 처리 */
+		pro();
+		/* 출력 */
+		if (isPossible) System.out.println("YES");
+		else System.out.println("NO");
+	}
 }
