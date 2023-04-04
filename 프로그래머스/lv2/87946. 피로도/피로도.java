3e48dc2eed3eb836fdfc4a8d3b24d6095750e126
Seoyoung Lim <seoyoung1113@gmail.com>
2023-04-05 00:49:35 +0900

    [level 2] Title: 피로도, Time: 0.10 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -11,28 +11,34 @@ class Solution {
         N = dungeons.length;
         visit = new boolean[N];
         
-        // 시작점, 초기 피로도
+        /*
+        @param1 탐색한 던전갯수
+        @param2 현재 남아있는 피로도
+        @param3 방문한 던전갯수
+        */
         dfs(0, K, 0);
         
         return ans;
     }
     
     private void dfs(int depth, int pirodo, int visitCnt) {
+        // 목적지에 도달했는가? -> 던전 모두 체크(방문) 완료! 
         if (visitCnt == N) {
+            // 최대 던전 수를 갱신한다
             ans = Math.max(ans, depth);
-     
             return;
         }
         
+        // 모든 던전을 체크(방문) 한다
         for (int i = 0; i < N; i++) {
-            // 방문하지 않았으며, 최소 필요 피로도를 만족한다.
             if (!visit[i]) {
                 // 체크인
                 visit[i] = true;
        
-                // dfs 간다.
+                // 최소 필요 피로도를 만족했을 경우 --> 해당 던전을 방문O (depth + 1)
                 if (pirodo >= D[i][0])
                     dfs(depth + 1, pirodo - D[i][1], visitCnt + 1);
+                // 최소 필요 피로도를 만족하지 못한 경우 --> 해당 던전을 방문X (depth 그대로)
                 else 
                     dfs(depth, pirodo, visitCnt + 1);
                 
