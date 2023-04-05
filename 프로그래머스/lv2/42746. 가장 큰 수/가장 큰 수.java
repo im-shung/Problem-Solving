c1497d2c413215f8a53cd33c019ac41ebe5a3006
Seoyoung Lim <seoyoung1113@gmail.com>
2023-04-05 19:04:57 +0900

    [level 2] Title: 가장 큰 수, Time: 6.92 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -4,44 +4,60 @@ class Solution {
     
     static class Element implements Comparable<Element> {
         
-        String full;
+        int[] su; // 자릿수배열
+        String full; // 기존 수
         
-        public Element(String full) {
+        public Element(String full, int maxLen) {
             this.full = full;
+            
+            // 자릿수배열 생성
+            su = new int[maxLen];
+            for (int i = 0; i < maxLen; i++) {
+                if (i < full.length()) {
+                    su[i] = full.charAt(i) -'0'; // full.charAt(i) 는 0~9까지
+                } else { // 기존 수의 길이보다 maxLen이 클 때.. 이전 수를 넣는다.
+                    su[i] = su[i-1];
+                }
+            }
        }
         
         @Override
         public int compareTo(Element o) {
-            return Integer.parseInt(o.full + this.full) - Integer.parseInt(this.full + o.full);
+            int i;
+            for (i = 0; i < this.su.length; i++) {
+                if (o.su[i] != this.su[i]) break;
+            }
+            return o.su[i] - this.su[i];
         }
         
         public String toString() {
-            return "full="+full;
+            return "su="+Arrays.toString(su) +",full="+full;
         }
-        // return a+b < b+a
     }
     
     public String solution(int[] numbers) {
+        
         // int배열에서 최대 길이를 구한다.
         Arrays.sort(numbers);
         int n = numbers.length;
         int max = numbers[n-1];
-        if (max == 0) return "0";
+        int maxLen = (max+"").length();
 
         // test
-        // System.out.println(maxLen);
+        System.out.println(maxLen);
         
-        // 최대길이만큼의 자릿수배열을 갖는 Element 배열을 생성한다.
+        // 최대길이만큼의 자릿수배열을 갖는 Element를 생성한다.
         Element[] elements = new Element[n];
         for (int i = 0; i < n; i++) {
-            elements[i] = new Element(numbers[i] + "");
+            elements[i] = new Element(numbers[i] + "", maxLen);
         }
-
+        //test
+        System.out.println(Arrays.toString(elements));
         // 정렬 
         Arrays.sort(elements);
         
         //test
-        // System.out.println(Arrays.toString(elements));
+        System.out.println(Arrays.toString(elements));
         
         StringBuilder sb = new StringBuilder();
         for (Element e : elements) {
